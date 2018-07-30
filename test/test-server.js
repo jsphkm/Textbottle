2e4381db9bb1546da9a93b9f6eb6f35890b9e24b
jsphkm <jsphkm12@gmail.com>
2018-07-30 04:44:51 -0700

    Add a simple test and travis integration

---
+++ test/test-server.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+const {app} = require('../server');
+
+
+describe('server', function(){
+  it('should display hello world on GET', function(){
+    return chai
+      .request(app)
+      .get('')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      })
+  });
+});
