95d85f34382e3dff244cc3ecac7fc1ec04fc2b23
jsphkm <jsphkm12@gmail.com>
2018-08-04 15:33:33 -0700

    Add all CRUD endpoints

---
+++ accounts/index.js
@@ -0,0 +1,4 @@
+const {Accounts} = require('./models');
+const {router} = require('./router');
+
+module.exports = {Accounts, router};
--- server.js
+++ server.js
@@ -1,12 +1,76 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+app.use(express.json());
+app.use(morgan('common'));
 app.use(express.static('public'));
 
-if (require.main === module) {
-	app.listen(process.env.PORT || 8080, function(){
-		console.info(`App listening on ${this.address().port}`);
+mongoose.Promise = global.Promise;
+
+const {DATABASE_URL, PORT} = require('./config');
+
+const {router: accountsRouter} = require('./accounts');
+const {router: authRouter, localStrategy, jwtStrategy} = require('./auth');
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/accounts', accountsRouter);
+app.use('/api/auth', authRouter);
+
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
+
+app.use('*', function(req, res){
+	res.status(404).json({message: 'Not Found'});
+})
+
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, {useNewUrlParser: true}, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`App listening on port ${port}`);
+				resolve();
+			})
+				.on('error', err => {
+					mongoose.disconnect();
+					reject(err);
+				});
+		});
 	});
 }
 
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
 
-module.exports = {app};
+if (require.main === module) {
+	runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = {runServer, app, closeServer};
